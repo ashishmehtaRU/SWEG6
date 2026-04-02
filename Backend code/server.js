7f91c89a9fb5a4017c0ee3db79ab7d1cbfc30fbb
BrianRutkowski12 <bar186@scarletmail.rutgers.edu>
2026-04-02 17:54:13 -0400

    Update server.js

--- Backend code/server.js
+++ Backend code/server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const cors=require("cors")
+const path = require("path");
 const routes=require("./routes")
 
 const app=express()
